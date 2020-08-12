e5a549706b851dc7587def4996a98b6ebd171201
Battleboyzz9 <44595864+Battleboyzz9@users.noreply.github.com>
2020-08-13 11:30:21 +1200

    yessssss

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+# Just Trying Out Render
+# from flask import Flask, render_template/
+# app = Flask(__name__)
+# @app.route("/login")
+# def login():
+#    return render_template('login.html')
+
 from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
