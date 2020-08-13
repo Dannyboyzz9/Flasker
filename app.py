f8f942e67cb737caac98a84b1e44d11d45469ef4
Battleboyzz9 <44595864+Battleboyzz9@users.noreply.github.com>
2020-08-13 13:55:40 +1200

    Alot of Stuff

--- app.py
+++ app.py
@@ -7,9 +7,14 @@
 
 from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 @app.errorhandler(404)
 def error404(e):
