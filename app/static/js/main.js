266d58bc697b6dd059cd628877fc5261b7b48e2a
Battleboyzz9 <44595864+Battleboyzz9@users.noreply.github.com>
2020-10-22 10:09:15 +1300

    Removed Unused Styling and made functionality Changes

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -11,18 +11,12 @@ $(document).ready(function(){
 $('.brick').click(function(){
 	var image_data = $(this).data(image);
 	var editable = $(this).hasClass('editable');
-
-	//This is the code that im using to 
-	var avatar_img = (typeof image.user_avatar === "undefined") ? `<img class="avatar" src="../static/img/blankavatar.jpg" alt="${image.user_name }">` : `<img class="avatar" src="${image.user_avatar}" alt="${image.user_name }">`;
-	
 	var image = image_data.image;
 	var edit_img = (editable) ? `<a href="/images/edit/${image.id}"><i class="fas fa-edit"></i></a>` : "";
 	var img = `<img class="modal-img" src="${image.upload_location}" alt="${image.name}">`;
 	var content = `<div class="imginfo">
-					`// ${avatar_img} //
-					`
 					<h5 class="modal-title">${image.name}> 
-						<a href="/images/like/${image.id}"><i class="like far fa-heart" data-image="${image.id}"></i></a>
+					<a href="/images/like/{{ image.id }}"><i class="far fa-heart"></i></a>
 						${edit_img}
 						<i class="fa fa-times" data-dismiss="modal" aria-label="Close" aria-hidden="true"></i>
 					</h5>
@@ -34,6 +28,7 @@ $('.brick').click(function(){
 	$('.modal').modal('show');
 });
 
+//{{ liked if image.id in likes else not_liked }}
 
 $(document).ready(function(){
 	
@@ -55,24 +50,33 @@ $(document).ready(function(){
 				image_id: image_id
 			}, 
 			function(result) {
-				//code to change the like icon
-				_this.removeClass('some-class') 
-				_this.removeClass('another-class')
+				if (result){
+					if (like) {
+						_this.removeClass('far'); 
+						_this.addClass('fas');
+					} else {
+						_this.removeClass('fas') 
+						_this.addClass('far');
+					}
+				}
+
 			}
 		);
 	});
 
 });
-//var yes = String;
-//yes = document.getElementsByName(`${image.user_avatar}`).value;
-//if (typeof image.user_avatar !== 'undefined'){ //checks whether the user avatar has a defined string eg does it have a link
-//	image.src = "../static/img/blankavatar.jpg"; //this will set the avatar_img to the link of the avatar if it exists 
-// } else {
-//	image.src = `${image.user_avatar}`; //this will set the avatar_img to a default img
+
+
+
+////This is the code that im using to 
+//	var avatar_img = (typeof image.user_avatar === "undefined") ? `<img class="avatar" src="../static/img/blankavatar.jpg" alt="${image.user_name }">` : `<img class="avatar" src="${image.user_avatar}" alt="${image.user_name }">`;
+//
+//if (condition) {
+//	_this.removeClass('far')
+//} else {
+//	_this.removeClass('fas') 
 //}
-//var avalible = $(this).('a')
-//var avatar_img = (avalible) ? `<img class="avatar" src="../static/img/blankavatar.jpg" alt="${image.user_name }"></img>` : `<img class="avatar" src="${image.user_avatar}" alt="${image.user_name }"></img>`;
-// this code will be like hey does this person have an avatar or a file uploaded to user.avatar 
-// or whether it has or has not got a string associated with it
-// var avatar_img_avalility = $(this).
-// var user_avatar = `${image.user_avatar}`; sets user_avatar as the users avatar00
+//
+//code to change the like icon
+//_this.removeClass('fas') 
+//_this.removeClass('far')
