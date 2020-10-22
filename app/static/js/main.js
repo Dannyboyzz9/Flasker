ffa3bdc64b258532c12304028def1fdfe7d1da03
Battleboyzz9 <44595864+Battleboyzz9@users.noreply.github.com>
2020-10-22 14:12:15 +1300

    Functionality Changes

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -8,12 +8,24 @@ $(document).ready(function(){
 	});
 });
 
-$('.brick').click(function(){
-	var image_data = $(this).data(image);
+/** The Following code listens for the figure and associated Image under the class brick to be clicked.
+ * When clicked image data and scr are gathered from firebase and set as local variables.
+ * Modal content is then assinged with preassinged infomation with classes already assined 
+ * and variables are set to infomation that needs to be changed from each image as there are multiple 
+ * images and each one is unique. Python tags like ${image.upload_location} set the image to the correct image
+ * and recive the scr infomation from firebase. All the elements are then added together then displayed
+ * as a overlay under the class 'modal'
+ */
+$('.brick figure').click(function(){
+	var image_data = $(this).closest('.brick').data(image);
 	var editable = $(this).hasClass('editable');
 	var image = image_data.image;
 	var edit_img = (editable) ? `<a href="/images/edit/${image.id}"><i class="fas fa-edit"></i></a>` : "";
-	var img = `<img class="modal-img" src="${image.upload_location}" alt="${image.name}">`;
+
+	var img = `	<figure class="filter-${image.filter}">
+					<img class="modal-img" src="${image.upload_location}" alt="${image.name},${image.description}">
+				</figure>`;
+
 	var content = `<div class="imginfo">
 					<h5 class="modal-title">${image.name}> 
 					<a href="/images/like/{{ image.id }}"><i class="far fa-heart"></i></a>
@@ -30,10 +42,37 @@ $('.brick').click(function(){
 
 //{{ liked if image.id in likes else not_liked }}
 
-$(document).ready(function(){
-	
-	//some other code
+/**
+ * The Following code will make sure that the correct values are loaded on to 
+ */
 
+if ($('#filter-select').length > 0 ) {
+	var filter = $('#filter-select').data('filter');
+	$('#filter-select').val(filter);
+}
+
+if ($('#category').length > 0 ) {
+	var category = $('#category').data('category');
+	$('#category').val(category);
+}
+
+$('#fliter-select').change(function(e) {
+	var new_filter = 'filter-' + this.value;
+	$('#image figure').removeClass();
+	$('#image figure').addClass(new_filter);
+});
+
+
+/**  The following code is used for the functionality of the liking system
+ *   The Code waits for a user to click the like button then sets the class/visual represnetation 
+ * based on whether the like button has or has not been prevously been pressed. Once the like button is
+ * pressed a JSON request is made to firebase and dep
+ * 
+ */
+
+$(document).ready(function(){
+
+	
 	$('i.like').click(function(e) {
 	
 		e.stopPropagation();
@@ -55,7 +94,7 @@ $(document).ready(function(){
 						_this.removeClass('far'); 
 						_this.addClass('fas');
 					} else {
-						_this.removeClass('fas') 
+						_this.removeClass('fas');
 						_this.addClass('far');
 					}
 				}
@@ -65,18 +104,3 @@ $(document).ready(function(){
 	});
 
 });
-
-
-
-////This is the code that im using to 
-//	var avatar_img = (typeof image.user_avatar === "undefined") ? `<img class="avatar" src="../static/img/blankavatar.jpg" alt="${image.user_name }">` : `<img class="avatar" src="${image.user_avatar}" alt="${image.user_name }">`;
-//
-//if (condition) {
-//	_this.removeClass('far')
-//} else {
-//	_this.removeClass('fas') 
-//}
-//
-//code to change the like icon
-//_this.removeClass('fas') 
-//_this.removeClass('far')
